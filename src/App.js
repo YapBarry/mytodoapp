d8116744cd4321de940c495e9f516472c396f6f6
Barry Yap <barry_yap@aiap.sg>
2022-10-31 15:56:07 +0800

    added firebase functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
-import {useState} from 'react';
-
-// for firebase functionality/ saving functionality
+import {useState, useEffect} from 'react';
 import { db } from "./firebase";
 import { collection, addDoc } from "firebase/firestore";
 
-// is there e prevent default in here? ########## TODO
-// Add use effect here ########## TODO
-
+import { query, onSnapshot, doc, updateDoc, deleteDoc, where } from "firebase/firestore";
 
 export function App() {
   const [list, setList] =useState([]);
-  const [input, setInput] = useState(""); //title
+  const [input, setInput] = useState("");
+  const collectionRef = collection(db, 'todo');
+  
+  useEffect(() => {
+    
+    const q = query(collectionRef); //where clause if necessary
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todoArray = [];
+      querySnapshot.forEach((doc) => {
+        todoArray.push({...doc.data(), id: doc.id});
+      });
+      setList(todoArray);
+    })
+    return () => unsub();
+  }, []);
 
-  // supposedly handle to do
-  // https://www.koderhq.com/tutorial/vue/firestore-database/
-  // addDoc(collectionReference, dataObject)
-  // collection(firestoreInstance, 'collectionName')
   const addToDo = async (toDo) => {
-    // title is input // input is input
-    // input is e.target.value
     if (input !== "") {
       const newToDo = {
-        id: Math.random(),
         toDo: toDo,
         completed: false
       };
 
-      // add todo to database (for database method)
-      const collectionRef = collection(db, 'todo');
       await addDoc(collectionRef, newToDo);
 
       // // add the todo to existing list (for non database method)
@@ -38,16 +39,28 @@ export function App() {
     }
   };
 
-  const deleteToDo = (id) => {
-    const newList = list.filter(toDo => toDo.id !== id);
-    setList(newList);
-  }
+  // const deleteToDo = (id) => {
+  //   const newList = list.filter(toDo => toDo.id !== id);
+  //   setList(newList);
+  // }
 
-  // to apply strikethrough formatting to toDo if completed = true
-  const handleToggle = (id) => {
-    let mapped = list.map(toDo => toDo.id == id ? { ...toDo, completed: !toDo.completed } : { ...toDo});
-    setList(mapped);
-    console.log(mapped);
+  // // to apply strikethrough formatting to toDo if completed = true
+  // const handleToggle = (id) => {
+  //   let mapped = list.map(toDo => toDo.id == id ? { ...toDo, completed: !toDo.completed } : { ...toDo});
+  //   setList(mapped);
+  //   console.log(mapped);
+  // };
+
+  const handleEdit = async (todo, list) => { 
+    await updateDoc(doc(db, 'todo', todo.id), {toDo: list});
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, 'todo', todo.id), { 
+      completed: !todo.completed
+    });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, 'todo', id));
   };
 
   return (
@@ -62,9 +75,12 @@ export function App() {
       <ul>
         {list.map((toDo) => (
           <div key={toDo.id} className={toDo.completed ? "strike" : ""}>
-            <input value={toDo.toDo} type="checkbox" onClick={() => handleToggle(toDo.id)}/>
+            <input 
+            value={toDo.toDo} 
+            type="checkbox" 
+            onClick={() => toggleComplete(toDo.id)}/>
             <span>{toDo.toDo}</span>
-            <button onClick={() => deleteToDo(toDo.id)}>&times;</button>  
+            <button onClick={() => handleDelete(toDo.id)}>&times;</button>  
             <hr></hr>     
           </div>
         ))}
